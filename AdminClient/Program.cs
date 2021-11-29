c9c94c26893751442602aea44c1faefd1a08e068
GAlverus <70200801+GAlverus@users.noreply.github.com>
2021-11-29 12:14:11 +0100

    Börjar skrivat frontend fär adminLogin

--- AdminClient/Program.cs
+++ AdminClient/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Runtime.InteropServices;
+using System.Threading;
+using DataLayer.Backend;
+using DataLayer.Model;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+
+AdminBackend admin = new AdminBackend();
+admin.CreateAndSeedDb();
+Console.WriteLine("Database initialized");
+Thread.Sleep(2000);
+
+Console.WriteLine();
---
+++ AdminClient1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using DataLayer;
+using DataLayer.Backend;
+
+namespace AdminClient1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AdminBackend admin = new AdminBackend();
+            admin.CreateAndSeedDb();
+            Console.WriteLine("Database initialized");
+            Thread.Sleep(2000);
+
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Vänligen logga in med Användarnamn/Epost och Lösenord.");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Användarnamn/Epost: ");
+                var user = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Lösenord: ");
+                var password = Console.ReadLine();
+
+            }
+        }
+    }
+}
