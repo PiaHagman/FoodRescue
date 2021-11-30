104c9d0baf57dc78a0e067ffebb2bb8b300cb690
GAlverus <70200801+GAlverus@users.noreply.github.com>
2021-11-30 19:16:48 +0100

    Adminclient1 försvann i mergen men hittade den på ett annat ställe kopierade in koden i adminclient

--- AdminClient/Program.cs
+++ AdminClient/Program.cs
@@ -1,17 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO.Enumeration;
-using System.Runtime.InteropServices;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using DataLayer;
 using DataLayer.Backend;
 using DataLayer.Model;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminClient1
+{
+    public class Program
+    {
+        internal static void Main(string[] args)
+        {
+            //AdminBackend admin = new AdminBackend();
+            //admin.CreateAndSeedDb();
+            //Console.WriteLine("Database initialized");
+            //Thread.Sleep(2000);
+
+            //Console.WriteLine();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Inlogg Admin");
+                Console.WriteLine();
+                Console.Write("Användarnamn: ");
+                var userName = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Lösenord: ");
+                var password = Console.ReadLine();
+
+                //var user = UserBackend.TryLogin(userName, password);
+
+                try
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder();
+                    optionsBuilder.UseSqlServer(
+                        @"server=(localdb)\MSSQLLocalDB;database=FoodRescueDb_CodeFirst_Live");
+                    var login = new Login(optionsBuilder.Options);
+                    var user = login.User(userName, password);
+                    ProgramLoopLogin(user);
+
+                }
+                catch (Exception)
+                {
+
+                }
 
 
-AdminBackend admin = new AdminBackend();
-admin.CreateAndSeedDb();
-Console.WriteLine("Database initialized");
-Thread.Sleep(2000);
+            }
 
-Console.WriteLine();
+        }
+
+        public static void ProgramLoopLogin(UserPersonalInfo user)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Hej och välkommen {user.FullName}!");
+            }
+        }
+    }
+
+
+}
